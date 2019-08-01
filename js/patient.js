a416334d82a9a23bd71b7f9611c597cada50cf9e
D Nishiyama <nishiyama.daiki.tu@alumni.tsukuba.ac.jp>
2019-08-01 14:15:22 +0900

    文字列からqrコード生成

---
+++ js/patient.js
@@ -0,0 +1,9 @@
+$(function(){
+    $('#qrcode').qrcode({
+        text: 'hogehoge',
+        width: 200,
+        height: 200,
+        background : "#fff",
+        foreground : "#000"
+    });
+});
