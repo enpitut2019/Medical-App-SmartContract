fda489c33dfe4bcfa406cc51efd1df8182f7841b
D Nishiyama <nishiyama.daiki.tu@alumni.tsukuba.ac.jp>
2019-08-02 10:13:50 +0900

    fix id name replace space to underbar

--- js/patient.js
+++ js/patient.js
@@ -1,7 +1,7 @@
 $(function () {
     $("button[name='size']").on("click", function (e) {
         e.preventDefault();
-        var source = $('#name').val() + ',' + $('#country').val() + ',' + $('#language').val() + ',' + $('#destination').val() + ',' + $('#work place').val() + ',' + $('#length of stay').val() + ',' + $('#medical insurance').val() + ',' + $('#method of paymnt').val() + ',' + $('#religious requests').val() + ',' + $('#emergency contact').val() + ',' + $('#acquaintance').val() + ',' + $('#others').val();
+        var source = $('#name').val() + ',' + $('#country').val() + ',' + $('#language').val() + ',' + $('#destination').val() + ',' + $('#work_place').val() + ',' + $('#length_of_stay').val() + ',' + $('#medical_insurance').val() + ',' + $('#method_of_paymnt').val() + ',' + $('#religious_requests').val() + ',' + $('#emergency_contact').val() + ',' + $('#acquaintance').val() + ',' + $('#others').val();
         source = Encoding.convert(source, 'SJIS');
 
         try {
